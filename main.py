4b9c1781d70df9e665a3dad01949a536cc69a236
Sean McLellan <sean@baristalabs.io>
2024-01-22 20:33:53 -0500

    Refactor code structure

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pynput.mouse import Button, Controller as MouseController
 from colorama import Style, Fore, init as colorama_init
 import time
 from StreamDeck.DeviceManager import DeviceManager
+from modules.charge_blade import ChargeBlade
 
 from modules.deckard import update_key_image
 
@@ -34,389 +35,80 @@ keyboard = KeyboardController()
 mouse = MouseController()
 currentDeck = None
 
-# Bank the Phials, aka Charge Phials
-def bank_it(solo=False):
-    if solo:
-        start.play()
-    # R+B
-    mouse.press(Button.x2)
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    mouse.release(Button.x2)
-    if solo:
-        stop.play()
-
-def fill_phials(solo=False):
-    if solo:
-        start.play()
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(0.5) # Since we're lunging, we're slightly slower to the charge.
-
-    # Hold B
-    mouse.press(Button.right)
-    sleep(1)
-    mouse.release(Button.right)
-    sleep(0.25)
-
-    # Y
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    sleep(0.5)
-
-    # R+B
-    bank_it()
-
-    if solo:
-        stop.play()
-
-def fill_phials_2(solo=False):
-    if solo:
-        start.play()
-    # Y + Hold B (release) + Y + (Hold RT + B)
-    
-    # Y + Hold B (release)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    sleep(0.25)
-
-    # Hold B
-    mouse.press(Button.right)
-    sleep(1)
-    mouse.release(Button.right)
-    sleep(0.25)
-
-    # Y
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    sleep(0.5)
-
-    # R+B
-    bank_it()
-
-    if solo:
-        stop.play()
-
-def charge_shield(solo=False):
-    if solo:
-        start.play()
-        mouse.press(Button.x1)
-        sleep(0.25)
-        mouse.release(Button.x1)
-        sleep(1)
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(1)
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(0.5)
-
-    # R2
-    mouse.press(Button.x2)
-    sleep(0.25)
-    mouse.release(Button.x2)
-    if solo:
-        stop.play()
-
-def charge_sword(solo=False):
-    if solo:
-        start.play()
-    # R+B
-    mouse.press(Button.x2)
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    mouse.release(Button.x2)
-    sleep(0.5)
-
-    # Hold Y
-    mouse.press(Button.left)
-    sleep(2)
-    mouse.release(Button.left)
-    if solo:
-        stop.play()
-
-def charge_blade_sword_combo(solo=False):
-    if solo:
-        start.play()
-    # Y+B, hold B, Y, R+B, Y+B, Y+B, Y+B, R2, R+B, hold Y
-    # Left+Right, Hold Right, Left, Ctrl+Right, Left+Right, Left+Right, Ctrl, Ctrl+Left, Hold Left then release
-
-    fill_phials()
-    sleep(0.25)
-
-    # Charge Shield
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(1) # If we don't wait long enough, it does an elemental discharge
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(0.5) # If we don't wait long enough, it completes the SAED
-
-    # This cancels the SAED, charging the shield
-    mouse.press(Button.x2)
-    sleep(1.25) # If we don't wait long enough, we go into a morph slash
-    # Then we charge the sword
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    mouse.press(Button.left)
-    sleep(2.5) # The charge ls lengthy, if we don't wait long enough, it's just a return slash
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-
-    if solo:
-        stop.play()
-
-def charge_blade_sword_combo_2(solo=False):
-    if solo:
-        start.play()
-    # Y + Hold B (release) + Y + (Hold RT + B) + Y + Hold B (Release) + (Y+B) + (Y+B) + (Hold RT + B + Hold Y + Release)
-    
-    fill_phials_2()
-    sleep(0.25)
-
-    # Y + Hold B (release)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    sleep(1.25)
-
-    mouse.press(Button.right)
-    sleep(0.5)
-    mouse.release(Button.right)
-    sleep(1)
-
-    # Charge Shield
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(0.5)
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(0.5)
-
-    # This cancels the SAED, charging the shield
-    mouse.press(Button.x2)
-    sleep(1)
-    # Then we charge the sword
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    sleep(0.5)
-    mouse.press(Button.left)
-    sleep(2) # The charge ls lengthy
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-
-    if solo:
-        stop.play()
-
-def saed_combo(solo=False):
-    if solo:
-        start.play()
-
-    # Morph to Axe
-    # R+Y
-    mouse.press(Button.x2)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-    sleep(1.5)
-
-    # Y+B
-    mouse.press(Button.left)
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.right)
-    if solo:
-        stop.play()
-
-def saed_combo_2(solo: bool = False):
-    if solo:
-        start.play()
-    
-    # Morph to Axe
-    # R+Y
-    mouse.press(Button.x2)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-    sleep(2)
-
-    # B
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    sleep(1)
-
-    # B
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.right)
-    if solo:
-        stop.play()
-
-def savage_axe_combo(solo: bool = False):
-    if solo:
-        start.play()
-    # Morph to Axe
-    # R+Y
-    mouse.press(Button.x2)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-    sleep(2)
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(1)
-
-    # C
-    keyboard.press('c')
-    sleep(0.25)
-    keyboard.release('c')
-    sleep(3.0)
-
-    # Controversial, morph back to sword
-    mouse.press(Button.x2)
-    sleep(0.25)
-    mouse.release(Button.x2)
-
-    if solo:
-        stop.play()
-
-def savage_axe_finisher(solo: bool = False):
-    if solo:
-        start.play()
-    # Morph to Axe
-    # R+Y
-    mouse.press(Button.x2)
-    mouse.press(Button.left)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.x2)
-    sleep(2)
-
-    # Y+B
-    mouse.press(Button.x1)
-    sleep(0.25)
-    mouse.release(Button.x1)
-    sleep(1)
-
-    # C
-    keyboard.press('c')
-    sleep(0.25)
-    keyboard.release('c')
-    sleep(3.25)
-
-    # now we do the finisher
-    mouse.press(Button.left)
-    mouse.press(Button.right)
-    sleep(0.25)
-    mouse.release(Button.left)
-    mouse.release(Button.right)
-    
-    if solo:
-        stop.play()
-
-
+charge_blade = ChargeBlade(
+    mouse,
+    keyboard,
+    {"fanfare": fanfare, "bell": bell, "start": start, "stop": stop, "error": error},
+)
 actions = [
-  {
-    "index": 0,
-    "icon": "charge_sword.png",
-    "label": "Charge Sword",
-    "action": charge_sword,
-  },
-  {
-    "index": 1,
-    "icon": "charge_shield.png",
-    "label": "Charge Shield",
-    "action": charge_shield,
-  },
-  {
-    "index": 4,
-    "icon": "charge_phials.png",
-    "label": "Bank It",
-    "action": bank_it,
-  },
-  {
-    "index": 5,
-    "icon": "Fill_Phials_1.png",
-    "label": "Fill Phials Far",
-    "action": fill_phials,
-  },
-  {
-    "index": 6,
-    "icon": "Fill_Phials_2.png",
-    "label": "Fill Phials Near",
-    "action": fill_phials_2,
-  },
-  {
-    "index": 8,
-    "icon": "SAED_2.png",
-    "label": "SAED 2",
-    "action": saed_combo_2,
-  },
-  {
-    "index": 9,
-    "icon": "Savage_Axe_Finisher_2.png",
-    "label": "S. Axe Combo",
-    "action": savage_axe_combo,
-  },
-  {
-    "index": 10,
-    "icon": "Combo_1.png",
-    "label": "Full Combo Far",
-    "action": charge_blade_sword_combo,
-  },
-  {
-    "index": 11,
-    "icon": "Combo_2.png",
-    "label": "Full Combo Near",
-    "action": charge_blade_sword_combo_2,
-  },
-  {
-    "index": 13,
-    "icon": "SAED_1.png",
-    "label": "SAED",
-    "action": saed_combo,
-  },
-  {
-    "index": 14,
-    "icon": "Savage_Axe_Finisher_1.png",
-    "label": "S. Axe Finisher",
-    "action": savage_axe_finisher,
-  },
+    {
+        "index": 0,
+        "icon": "charge_sword.png",
+        "label": "Charge Sword",
+        "action": charge_blade.charge_sword,
+    },
+    {
+        "index": 1,
+        "icon": "charge_shield.png",
+        "label": "Charge Shield",
+        "action": charge_blade.charge_shield,
+    },
+    {
+        "index": 4,
+        "icon": "charge_phials.png",
+        "label": "Bank It",
+        "action": charge_blade.bank_it,
+    },
+    {
+        "index": 5,
+        "icon": "Fill_Phials_1.png",
+        "label": "Fill Phials Far",
+        "action": charge_blade.fill_phials,
+    },
+    {
+        "index": 6,
+        "icon": "Fill_Phials_2.png",
+        "label": "Fill Phials Near",
+        "action": charge_blade.fill_phials_2,
+    },
+    {
+        "index": 8,
+        "icon": "SAED_2.png",
+        "label": "SAED 2",
+        "action": charge_blade.saed_combo_2,
+    },
+    {
+        "index": 9,
+        "icon": "Savage_Axe_Finisher_2.png",
+        "label": "S. Axe Combo",
+        "action": charge_blade.savage_axe_combo,
+    },
+    {
+        "index": 10,
+        "icon": "Combo_1.png",
+        "label": "Full Combo Far",
+        "action": charge_blade.charge_blade_sword_combo,
+    },
+    {
+        "index": 11,
+        "icon": "Combo_2.png",
+        "label": "Full Combo Near",
+        "action": charge_blade.charge_blade_sword_combo_2,
+    },
+    {
+        "index": 13,
+        "icon": "SAED_1.png",
+        "label": "SAED",
+        "action": charge_blade.saed_combo,
+    },
+    {
+        "index": 14,
+        "icon": "Savage_Axe_Finisher_1.png",
+        "label": "S. Axe Finisher",
+        "action": charge_blade.savage_axe_finisher,
+    },
 ]
+
 
 # Prints key state change information, updates rhe key image and performs any
 # associated actions when a key is pressed.
@@ -424,20 +116,20 @@ def key_change_callback(deck, key, state):
     # Print new key state
     if debug:
         print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
-    
+
     # only do stuff if the key is pressed
     if state == False:
         return
- 
+
     keyAction = None
     for action in actions:
         if action["index"] == key:
             keyAction = action
             break
-    
+
     if keyAction is None:
         return
-    
+
     # Update the key image based on the new key state.
     update_key_image(deck, key, "Running.png", "Running...")
     # Perform the action
@@ -445,9 +137,12 @@ def key_change_callback(deck, key, state):
     # Update the key image based on the new key state.
     update_key_image(deck, key, keyAction["icon"], keyAction["label"])
 
-def main():
+
+def init_streamdeck():
     streamdecks = DeviceManager().enumerate()
-    print(f"Found {len(streamdecks)} Stream Deck(s). Usint the first visual Stream Deck.")
+    print(
+        f"Found {len(streamdecks)} Stream Deck(s). Using the first visual Stream Deck."
+    )
 
     # Get the first streamdeck where is_visual is True
     for deck in streamdecks:
@@ -462,10 +157,13 @@ def main():
     currentDeck.open()
     currentDeck.reset()
 
-    print("Opened '{}' device (serial number: '{}', fw: '{}')".format(
-            deck.deck_type(), deck.get_serial_number(), deck.get_firmware_version()
-        ))
-    
+    if debug:
+        print(
+            "Opened '{}' device (serial number: '{}', fw: '{}')".format(
+                deck.deck_type(), deck.get_serial_number(), deck.get_firmware_version()
+            )
+        )
+
     currentDeck.set_brightness(100)
 
     # Set initial key images.
@@ -478,8 +176,11 @@ def main():
                 break
         if keyAction is not None:
             update_key_image(deck, key, keyAction["icon"], keyAction["label"])
-    deck.set_key_callback(key_change_callback)
+    currentDeck.set_key_callback(key_change_callback)
+    return currentDeck
 
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--debug", action="store_true")
     args = parser.parse_args()
@@ -488,8 +189,9 @@ def main():
     debug = args.debug
     print(f"Debug: {debug}")
 
-    print(f"{Fore.LIGHTMAGENTA_EX}Starting Auto MH:World ...{Style.RESET_ALL}")
+    currentDeck = init_streamdeck()
 
+    print(f"{Fore.LIGHTMAGENTA_EX}Starting Auto MH:World ...{Style.RESET_ALL}")
 
     if args.debug:
         print(f"\t{Fore.LIGHTGREEN_EX}Debugging mode enabled{Style.RESET_ALL}")
---
+++ modules/charge_blade.py
@@ -0,0 +1,325 @@
+from pynput.mouse import Button
+from time import sleep
+
+
+class ChargeBlade:
+    def __init__(self, mouse, keyboard, sounds):
+        self.mouse = mouse
+        self.keayboard = keyboard
+        self.sounds = sounds
+
+    # Bank the Phials, aka Charge Phials
+    def bank_it(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+        # R+B
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        self.mouse.release(Button.x2)
+        if solo:
+            self.sounds["stop"].play()
+
+    def fill_phials(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(0.5)  # Since we're lunging, we're slightly slower to the charge.
+
+        # Hold B
+        self.mouse.press(Button.right)
+        sleep(1)
+        self.mouse.release(Button.right)
+        sleep(0.25)
+
+        # Y
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        sleep(0.5)
+
+        # R+B
+        self.bank_it()
+
+        if solo:
+            self.sounds["stop"].play()
+
+    def fill_phials_2(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+        # Y + Hold B (release) + Y + (Hold RT + B)
+
+        # Y + Hold B (release)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        sleep(0.25)
+
+        # Hold B
+        self.mouse.press(Button.right)
+        sleep(1)
+        self.mouse.release(Button.right)
+        sleep(0.25)
+
+        # Y
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        sleep(0.5)
+
+        # R+B
+        self.bank_it()
+
+        if solo:
+            self.sounds["stop"].play()
+
+    def charge_shield(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+            self.mouse.press(Button.x1)
+            sleep(0.25)
+            self.mouse.release(Button.x1)
+            sleep(1)
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(1)
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(0.5)
+
+        # R2
+        self.mouse.press(Button.x2)
+        sleep(0.25)
+        self.mouse.release(Button.x2)
+        if solo:
+            self.sounds["stop"].play()
+
+    def charge_sword(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+        # R+B
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        self.mouse.release(Button.x2)
+        sleep(0.5)
+
+        # Hold Y
+        self.mouse.press(Button.left)
+        sleep(2)
+        self.mouse.release(Button.left)
+        if solo:
+            self.sounds["stop"].play()
+
+    def charge_blade_sword_combo(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+        # Y+B, hold B, Y, R+B, Y+B, Y+B, Y+B, R2, R+B, hold Y
+        # Left+Right, Hold Right, Left, Ctrl+Right, Left+Right, Left+Right, Ctrl, Ctrl+Left, Hold Left then release
+
+        self.fill_phials()
+        sleep(0.25)
+
+        # Charge Shield
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(1)  # If we don't wait long enough, it does an elemental discharge
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(0.5)  # If we don't wait long enough, it completes the SAED
+
+        # This cancels the SAED, charging the shield
+        self.mouse.press(Button.x2)
+        sleep(1.25)  # If we don't wait long enough, we go into a morph slash
+        # Then we charge the sword
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        self.mouse.press(Button.left)
+        sleep(
+            2.5
+        )  # The charge ls lengthy, if we don't wait long enough, it's just a return slash
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+
+        if solo:
+            self.sounds["stop"].play()
+
+    def charge_blade_sword_combo_2(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+        # Y + Hold B (release) + Y + (Hold RT + B) + Y + Hold B (Release) + (Y+B) + (Y+B) + (Hold RT + B + Hold Y + Release)
+
+        self.fill_phials_2()
+        sleep(0.25)
+
+        # Y + Hold B (release)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        sleep(1.25)
+
+        self.mouse.press(Button.right)
+        sleep(0.5)
+        self.mouse.release(Button.right)
+        sleep(1)
+
+        # Charge Shield
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(0.5)
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(0.5)
+
+        # This cancels the SAED, charging the shield
+        self.mouse.press(Button.x2)
+        sleep(1)
+        # Then we charge the sword
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        sleep(0.5)
+        self.mouse.press(Button.left)
+        sleep(2)  # The charge ls lengthy
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+
+        if solo:
+            self.sounds["stop"].play()
+
+    def saed_combo(self, solo=False):
+        if solo:
+            self.sounds["start"].play()
+
+        # Morph to Axe
+        # R+Y
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+        sleep(1.5)
+
+        # Y+B
+        self.mouse.press(Button.left)
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.right)
+        if solo:
+            self.sounds["stop"].play()
+
+    def saed_combo_2(self, solo: bool = False):
+        if solo:
+            self.sounds["start"].play()
+
+        # Morph to Axe
+        # R+Y
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+        sleep(2)
+
+        # B
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        sleep(1)
+
+        # B
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.right)
+        if solo:
+            self.sounds["stop"].play()
+
+    def savage_axe_combo(self, solo: bool = False):
+        if solo:
+            self.sounds["start"].play()
+        # Morph to Axe
+        # R+Y
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+        sleep(2)
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(1)
+
+        # C
+        self.keyboard.press("c")
+        sleep(0.25)
+        self.keyboard.release("c")
+        sleep(3.0)
+
+        # Controversial, morph back to sword
+        self.mouse.press(Button.x2)
+        sleep(0.25)
+        self.mouse.release(Button.x2)
+
+        if solo:
+            self.sounds["stop"].play()
+
+    def savage_axe_finisher(self, solo: bool = False):
+        if solo:
+            self.sounds["start"].play()
+        # Morph to Axe
+        # R+Y
+        self.mouse.press(Button.x2)
+        self.mouse.press(Button.left)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.x2)
+        sleep(2)
+
+        # Y+B
+        self.mouse.press(Button.x1)
+        sleep(0.25)
+        self.mouse.release(Button.x1)
+        sleep(1)
+
+        # C
+        self.keyboard.press("c")
+        sleep(0.25)
+        self.keyboard.release("c")
+        sleep(3.25)
+
+        # now we do the finisher
+        self.mouse.press(Button.left)
+        self.mouse.press(Button.right)
+        sleep(0.25)
+        self.mouse.release(Button.left)
+        self.mouse.release(Button.right)
+
+        if solo:
+            self.sounds["stop"].play()
